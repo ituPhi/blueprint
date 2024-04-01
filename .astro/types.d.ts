b467d89142776f3b70e9a051510840a2ad9c857b
ituPhi <jfiturbeo@gmail.com>
2024-04-01 15:25:50 -0500

    new post

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -126,6 +126,13 @@ declare module 'astro:content' {
 
 	type ContentEntryMap = {
 		"proyectos": {
+"posts/mi-post-nuevo-2.md": {
+	id: "posts/mi-post-nuevo-2.md";
+  slug: "posts/mi-post-nuevo-2";
+  body: string;
+  collection: "proyectos";
+  data: InferEntrySchema<"proyectos">
+} & { render(): Render[".md"] };
 "posts/project-01.md": {
 	id: "posts/project-01.md";
   slug: "posts/project-01";
