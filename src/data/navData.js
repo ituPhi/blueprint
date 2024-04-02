522254770eea4794a7764d6c352c5e816fc8e0c2
ituPhi <jfiturbeo@gmail.com>
2024-04-02 10:16:51 -0500

    nav Data

---
+++ src/data/navData.js
@@ -0,0 +1,28 @@
+```json:src/data/navData.js
+[
+  {
+    "href": "/",
+    "label": "Inicio",
+    "active": "currentPath === ''"
+  },
+  {
+    "href": "/services",
+    "label": "Servicios",
+    "active": "currentPath === 'services'"
+  },
+  {
+    "href": "/proyectos",
+    "label": "Proyectos",
+    "active": "currentPath === 'proyectos'"
+  },
+  {
+    "href": "/acerca",
+    "label": "Acerca de",
+    "active": "currentPath === 'acerca'"
+  },
+  {
+    "href": "/contacto",
+    "label": "Contacto",
+    "active": "currentPath === 'contacto'"
+  }
+]
